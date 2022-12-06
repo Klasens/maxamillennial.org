e5a0a232dd7679da9d967baefada33f19715b849
Bob Klasen <robfklasen@gmail.com>
2022-12-05 20:30:45 -0600

    APhorisms and Poetry mobile 2

--- mediaQueries.js
+++ mediaQueries.js
@@ -41,17 +41,19 @@ function displayNonfictionSFList(mediaQuery850) {
 function displayAphorismsList(mediaQuery850) {
   if (mediaQuery850.matches) {
     // If media query matches
-    console.log('test');
-    showSideMenu();
-    showAphorismsList();
+    mainLinks.classList.add('width-0');
+    mainLinks.classList.add('opacity-0');
+    pieceLinks.classList.add('width-100');
+    aphorismClose.classList.remove('opacity-0');
   }
 }
 function displayPoetryList(mediaQuery850) {
   if (mediaQuery850.matches) {
     // If media query matches
-    console.log('test');
-    showSideMenu();
-    showPoetryList();
+    mainLinks.classList.add('width-0');
+    mainLinks.classList.add('opacity-0');
+    pieceLinks.classList.add('width-100');
+    poemClose.classList.remove('opacity-0');
   }
 }
 
@@ -69,24 +71,21 @@ tocSFLink.addEventListener('click', function () {
   displayNonfictionSFList(mediaQuery850); // Call listener function at run time
 });
 tocAphorismsLink.addEventListener('click', function () {
-  console.log('test');
-  mainLinks.classList.add('width-0');
-  pieceLinks.classList.add('width-100');
-  aphorismClose.classList.remove('opacity-0');
+  displayAphorismsList(mediaQuery850);
 });
 tocPoetryLink.addEventListener('click', function () {
-  mainLinks.classList.add('width-0');
-  pieceLinks.classList.add('width-100');
-  poemClose.classList.remove('opacity-0');
+  displayPoetryList(mediaQuery850);
 });
 
 aphorismClose.addEventListener('click', function () {
   mainLinks.classList.remove('width-0');
+  mainLinks.classList.remove('opacity-0');
   pieceLinks.classList.remove('width-100');
-  aphorismClose.classList.toggle('opacity-0');
+  aphorismClose.classList.add('opacity-0');
 });
 poemClose.addEventListener('click', function () {
   mainLinks.classList.remove('width-0');
+  mainLinks.classList.remove('opacity-0');
   pieceLinks.classList.remove('width-100');
-  poemClose.classList.toggle('opacity-0');
+  poemClose.classList.add('opacity-0');
 });
