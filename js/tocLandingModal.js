3518b17c91ae27a01de478281628db0cb84340bc
Bob Klasen <robfklasen@gmail.com>
2023-01-10 00:47:46 -0600

    Updated modal for landing page

---
+++ js/tocLandingModal.js
@@ -0,0 +1,40 @@
+const landingModal = document.querySelector('.modal-landing');
+const landingModalContainer = document.querySelector('.modal-container');
+const landingModalClose = document.querySelector('.link-2');
+const topNavTitle = document.querySelector('.topNav-title');
+
+topNavTitle.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+  console.log('test');
+  landingModal.classList.add('showModal');
+  landingModalContainer.classList.add('showModal');
+  landingModalContainer.classList.remove('pointerEvents-none');
+});
+
+landingModal.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    landingModal.classList.remove('showModal');
+    landingModalContainer.classList.remove('showModal');
+    landingModalContainer.classList.add('pointerEvents-none');
+  }
+});
+
+landingModalClose.addEventListener('click', function () {
+  landingModal.classList.remove('showModal');
+  landingModalContainer.classList.remove('showModal');
+  landingModalContainer.classList.add('pointerEvents-none');
+});
+
+outerModal.addEventListener('click', function () {
+  landingModal.classList.remove('showModal');
+  landingModalContainer.classList.remove('showModal');
+  landingModalContainer.classList.add('pointerEvents-none');
+});
